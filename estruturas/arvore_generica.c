e07b7b3101a795f3c1c7fcc0b8e24a4fcabadceb
danielpcampagna <danielprett@gmail.com>
2019-06-11 17:37:54 -0300

    Finaliza função de insere

--- estruturas/arvore_generica.c
+++ estruturas/arvore_generica.c
@@ -1,74 +1,59 @@
 #include "arvore_generica.h"
 
-
-TAG* cria(){
-	TAG* p= malloc(sizeof(TAG));
-	return NULL;
+TAG *cria()
+{
+    TAG *p = malloc(sizeof(TAG));
+    return NULL;
 }
 
-
-
-
-static TAG* busca(TAG* a, int x){
-if (a==NULL) return NULL;
-
-else if (a->id==x) return a;
-
-else {
-TAG* p= busca(a->irmao, x);
-if (p != NULL)
-return p;
-else return busca(a->filho, x);
+TAG *busca(TAG *a, int x)
+{
+    if (a == NULL) return NULL;
+    if (a->id == x) return a;
+    
+    TAG *p = busca(a->irmao, x);
+    
+    if (p != NULL) return p;
+    return busca(a->filho, x);
 }
 
+void insere(TAG *a, TAG *no, int pai)
+{
+
+    if(!a) return a;
+    
+    TAG *no = malloc(sizeof(TAG));
+    no->filho = NULL;
+    no->irmao = NULL;
+
+    TAG *p = busca(a, pai);
+    if(!p) return NULL;
+    if(!p->filho) p->filho = no;
+    p = p->filho;
+    while(p->irmao) p = p->irmao;
+    p->irmao = no;
 }
 
-
-
-
-void insere(TAG* a, TAG* no, int pai ){
-
-if (a!=NULL) {
-	TAG* no= malloc(sizeof(TAG));
-	TAG* p=busca(a, pai);
-	if (p->filho==NULL) {
-		no->filho=NULL;
-		no->irmao=NULL;
-		p->filho=no;
-	}
-	else {
-		p=p->filho;
-		while (p) {
-			p=p->irmao;
-		}
-		no->filho=NULL;
-		no->irmao=NULL;
-		p->irmao=no;
-	}
-
-}
-
-}
-
-
-
-void imprime(TAG *t){
-     TAG* result = NULL;
+void imprime(TAG *t)
+{
+    TAG *result = NULL;
     return result;
 }
 
-
-
-TAG* retira(TAG *t, int id) {
-    TAG* result = NULL;
+TAG *retira(TAG *t, int id)
+{
+    TAG *result = NULL;
     return result;
 }
 
-void destroi(TAG *t) {
+void destroi(TAG *t)
+{
     return;
 }
 
-TAG* altera(TAG *t, int id, void *altera_fig(), ...){
-    TAG* result = NULL;
+TAG *altera(TAG *t, int id, void *altera_fig(), ...)
+{
+    TAG *result = NULL;
     return result;
 }
+
